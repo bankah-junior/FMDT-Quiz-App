cfdb3dc8cb99fd171d1dd27e09604cb08e6809f0
bankah-junior <anthonybekoebankah@gmail.com>
2025-08-04 02:41:25 +0000

    Add About Me screen and integrate navigation from Onboarding screen

---
+++ lib/screens/about_me_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AboutMeScreen extends StatefulWidget {
+  const AboutMeScreen({super.key});
+
+  @override
+  State<AboutMeScreen> createState() => _AboutMeScreenState();
+}
+
+class _AboutMeScreenState extends State<AboutMeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Text("About Me Screen"),
+    );
+  }
+}
